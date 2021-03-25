2e007146c76377a7ce4521530c983396368c8927
Moritz <moritzilg@hotmail.com>
2021-03-25 20:55:24 +0100

    simulate ai vs random player

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from Backgammon import Checker, Game
-from Player import HumanPlayer
+from Backgammon import Checker, Board, Game
+from Player import AiPlayer, RandomPlayer
+from mapper import NNMapper
 
-player_1 = HumanPlayer(Checker.WHITE)
-player_2 = HumanPlayer(Checker.BLACK)
-game = Game(player_1, player_2, True)
-game.run()
-# die = Die(5, 6)
-# moves_1 = move_generator.generate_moves(player_1, die, game.board.get_view())
-# moves_2 = move_generator.generate_moves(player_2, die, game.board.get_view(True))
-# print(moves_1)
-# print(moves_1 == moves_2)
+if __name__ == '__main__':
+    node_mapper = NNMapper()
+    player1 = AiPlayer(Checker.WHITE)
+    player2 = RandomPlayer(Checker.BLACK)
+    board = Board(player1, player2)
+
+    game = Game(player_1=player1, player_2=player2, create_protocol=True)
+    game.run()
